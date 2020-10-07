2b77ecd94d28ecd7406b51c3944c9fd8a7f36a75
Muhammad Abdullah Khan <55477609+Abdullah0332@users.noreply.github.com>
2020-10-07 13:52:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,6 +101,7 @@ class App extends Component {
   render(){
     return(
       <div className="App">
+          <h1>Weather App</h1>
           <Form onSubmit={this.getWeatherInfo} error={this.state.error} />
           <Weather 
             country={this.state.country}
